c199afcf31ae8e70b9d367f9b83662350a711440
am502 <antegzeperob@gmail.com>
2019-03-21 04:00:55 +0300

    add method to article dao

--- src/main/java/ru/itis/dao/ArticleDao.java
+++ src/main/java/ru/itis/dao/ArticleDao.java
@@ -13,6 +13,9 @@ public class ArticleDao {
     private static final String INSERT_ARTICLE = "INSERT INTO articles (title, keywords, content, url, student_id) " +
             "VALUES (:title, :keywords, :content, :url, :studentId);";
     private static final String GET_ALL_ARTICLES = "SELECT * FROM articles;";
+    private static final String GET_URLS = "SELECT a.url FROM articles a WHERE a.id IN " +
+            "(SELECT at.article_id FROM article_term at INNER JOIN terms_list t ON at.term_id = t.term_id " +
+            "WHERE t.term_text = '";
 
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
@@ -27,5 +30,31 @@ public class ArticleDao {
 
     public List<Article> getAllArticles() {
         return namedParameterJdbcTemplate.query(GET_ALL_ARTICLES, new BeanPropertyRowMapper<>(Article.class));
+    }
+
+    public List<String> getUrlsByWords(List<String> words) {
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT term FROM words_porter WHERE term IN (");
+        for (String word : words) {
+            query.append("'");
+            query.append(word);
+            query.append("',");
+        }
+        query.deleteCharAt(query.length() - 1);
+        query.append(") GROUP BY (term) ORDER BY (COUNT(term));");
+        List<String> sortedWords = namedParameterJdbcTemplate
+                .getJdbcTemplate().queryForList(query.toString(), String.class);
+
+        query = new StringBuilder();
+        for (int i = 0; i < sortedWords.size() - 1; i++) {
+            query.append(GET_URLS);
+            query.append(sortedWords.get(i));
+            query.append("') INTERSECT ");
+        }
+        query.append(GET_URLS);
+        query.append(sortedWords.get(sortedWords.size() - 1));
+        query.append("');");
+        return namedParameterJdbcTemplate
+                .getJdbcTemplate().queryForList(query.toString(), String.class);
     }
 }
--- src/main/java/ru/itis/task3/BooleanSearch.java
+++ src/main/java/ru/itis/task3/BooleanSearch.java
@@ -1,14 +1,13 @@
 package ru.itis.task3;
 
+import ru.itis.dao.ArticleDao;
 import ru.itis.util.StemProcessor;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class BooleanSearch {
     public static void main(String[] args) {
-        Dao dao = new Dao();
-
         String text = "Обзор хорошей игры";
 
         List<String> processedWords = new ArrayList<>();
@@ -16,13 +15,8 @@ public class App {
             processedWords.add(StemProcessor.getInstance().processPorterStem(word.toLowerCase()));
         }
 
-        List<String> sortedWords = dao.getSortedWords(processedWords);
+        ArticleDao articleDao = new ArticleDao();
 
-        List<String> urls = dao.getUrls(sortedWords);
-
-        System.out.println("### URLS ###");
-        for (String url : urls) {
-            System.out.println(url);
-        }
+        articleDao.getUrlsByWords(processedWords).forEach(System.out::println);
     }
 }
