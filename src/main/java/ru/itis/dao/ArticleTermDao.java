95c500e7a56974abfed69984d977b9e7c6ad3c6f
am502 <antegzeperob@gmail.com>
2019-03-24 14:51:09 +0300

    change task 5

--- src/main/java/ru/itis/dao/ArticleTermDao.java
+++ src/main/java/ru/itis/dao/ArticleTermDao.java
@@ -1,10 +1,12 @@
 package ru.itis.dao;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import ru.itis.config.DataConfig;
 import ru.itis.models.ArticleTerm;
+import ru.itis.models.ModifiedArticleTerm;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +17,9 @@ public class ArticleTermDao {
             "WHERE article_id = :articleId::UUID AND term_id = :termId::UUID;";
     private static final String GET_WORD_IDS_WITH_ARTICLE_IDS = "SELECT t.term_id, p.article_id " +
             "FROM words_porter p INNER JOIN terms_list t ON t.term_text = p.term;";
+    private static final String GET_ALL_MODIFIED_ARTICLE_TERMS = "SELECT at.tf_idf, at.idf, " +
+            "a.url AS article_url, t.term_text AS term FROM article_term at " +
+            "INNER JOIN articles a ON at.article_id = a.id INNER JOIN terms_list t ON t.term_id = at.term_id;";
 
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
@@ -48,5 +53,10 @@ public class ArticleTermDao {
             }
             return result;
         });
+    }
+
+    public List<ModifiedArticleTerm> getAllModifiedArticleTerms() {
+        return namedParameterJdbcTemplate
+                .query(GET_ALL_MODIFIED_ARTICLE_TERMS, new BeanPropertyRowMapper<>(ModifiedArticleTerm.class));
     }
 }
